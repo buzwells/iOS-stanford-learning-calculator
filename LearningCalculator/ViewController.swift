643b68690a25fc871860aa3cf1166b3bfc6f019f
Joel Wells <buzwells@gmail.com>
2015-08-11 20:08:44 -0500

    Added enter and operation support

--- LearningCalculator/ViewController.swift
+++ LearningCalculator/ViewController.swift
@@ -9,18 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
+
     @IBOutlet weak var calculationResult: UILabel!
     
     // true if the user has started entering a number
-    var dataEntryInProgress : Bool = false
+    var dataEntryInProgress = false
+    
+    var operands = Array<Double>()
     
     @IBAction func numberButtonClicked(sender: UIButton) {
-        println(sender.currentTitle!)
+        let digit = sender.currentTitle!
         if(dataEntryInProgress) {
-            calculationResult.text = calculationResult.text! + sender.currentTitle!
+            calculationResult.text = calculationResult.text! + digit
         } else {
-            calculationResult.text = sender.currentTitle!
+            calculationResult.text = digit
             dataEntryInProgress = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if dataEntryInProgress {
+            enter()
+        }
+        switch operation {
+            case "×": performOperation { $0 * $1 }
+            case "÷": performOperation { $1 / $0 }
+            case "+": performOperation { $0 + $1 }
+            case "−": performOperation { $1 - $0 }
+            case "√": performSingleOperation { sqrt($0) }
+            default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double ) -> Double) {
+        if operands.count > 1 {
+            displayValue = operation(operands.removeLast(), operands.removeLast())
+            enter()
+        }
+    }
+    
+    /* the lecture tried to demonstrate that Swift tolerates 
+       methods with the same name and return value but a different
+       number of args, but new compiler issues an error b/c this
+       class inherits from an Obj-C class, which is not so tolerant.
+       Makes this technique unsafe - so changed name of method.
+     */
+    func performSingleOperation(operation: Double -> Double) {
+        if operands.count > 0 {
+            displayValue = operation(operands.removeLast())
+            enter()
+        }
+    }
+    
+    @IBAction func enter() {
+        dataEntryInProgress = false
+        operands.append(displayValue)
+        println(operands)
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(calculationResult.text!)!.doubleValue
+        }
+        set {
+            calculationResult.text = "\(newValue)"
+            dataEntryInProgress = false
         }
     }
 }
